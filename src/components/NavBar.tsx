0baa34490524a9363d57265983b1dd6626f59efb
Scott Ertmer <scottertmer@gmail.com>
2024-04-25 15:59:48 -0600

    Add tailwind defaults

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,22 @@
+import Image from 'next/image';
+import backArrow from '../../public/003-rewind.png';
+import forwardArrow from '../../public/004-fast-forward.png';
+
 const NavBar = () => {
   return (
     <nav>
-      <p>this is the navbar component</p>
+      <Image
+      src={backArrow}
+      width={100}
+      height={100}
+      alt="back arrow"
+    />
+    <Image
+      src={forwardArrow}
+      width={100}
+      height={100}
+      alt="forward arrow"
+    />
     </nav>
   )
 }
